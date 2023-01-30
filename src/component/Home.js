4529f6699510fdd0329374752276ff93dd5b8d63
sumant <sumantdeve@gmail.com>
2023-01-30 15:39:25 +0530

    All funcionality completed

---
+++ src/component/Home.js
@@ -0,0 +1,66 @@
+// import react from 'react';
+
+import { useState } from "react";
+
+function Home(){
+    const [text, setText] = useState("");
+    
+    //handle onchange
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    //to change into uppercase
+    const handleUppercase = () => {
+         setText(text.toUpperCase());
+    }
+    //to change into lowercase
+    const handleLowercase = () => {
+        setText(text.toLowerCase());
+    }
+    //to remove all space
+    const removeSpace = () => {
+        setText(text.split(/[ ]+/).join(" "));
+    }
+    //copy text
+    const handleCopyText = () => {
+        var text = document.getElementById("textBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    // to clear text
+    const handleClear = () => {
+        setText("");
+    }
+    return(
+        <>
+            <div className="container mt-5">
+                <h1>Enter Text below:</h1>  
+                <div className="mb-2">
+                    <textarea id="textBox" value={text} onChange={handleOnChange} cols="100" rows="5" autoFocus></textarea>    
+                </div>
+                <button className="btn btn-primary mx-2" onClick={handleUppercase}>Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowercase}>Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={removeSpace}>Remove Space</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopyText}>Copy text</button>
+                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear text</button>
+
+                <div className="container mt-5 ">
+                    <div className="row">
+                        <div className="col-6">
+                            <h2>Preview</h2>
+                            <p>{text}</p>
+                        </div>
+                        <div className="col-6">
+                            <h2>About Text</h2>
+                            <p>Total Characters: {text.length}</p>
+                            <p>Total Words: {text === '' ? text.split(" ").length-1:text.split(" ").length}</p>
+                            <p>Total Sentences: {text === '' ? text.split(".").length-1:text.split(".").length}</p>
+                            <p>Time to read: {0.008 * text.length} minute.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Home;
